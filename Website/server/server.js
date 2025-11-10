0af287ca4677307ffed9931f51da207d7f586481
LIZI WANG <liziwang.uk@gmail.com>
2025-11-10 20:27:47 +0000

    Update server.js

--- Website/server/server.js
+++ Website/server/server.js
@@ -18,9 +18,9 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../front-end')));
+app.use(express.static(path.join(__dirname, '../client')));
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '../front-end/homepage/index.html'))
+  res.sendFile(path.join(__dirname, '../client/index.html'))
 );
 
 app.listen(3000, () => console.log('http://localhost:3000'));
